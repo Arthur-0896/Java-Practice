01711ad928c58f8d03bdbde0cb8c6b06464c93c8
Arthur <arthur.tristram1@gmail.com>
2025-10-04 19:47:03 -0400

    Added the Arrays.toList() demonstration

--- javaPractice/src/javaPractice/ListAndArrayConversions.java
+++ javaPractice/src/javaPractice/ListAndArrayConversions.java
@@ -1,11 +1,13 @@
 package javaPractice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ListAndArrayConversions {
 	
 	public static void main(String[] args) {
+		// toArray() method demonstration
 		List<String> arrayList= new ArrayList<>();
 		String[] arr = new String[3];
 		
@@ -22,11 +24,45 @@ public class ListAndArrayConversions {
 		for(String str : arr) {
 			System.out.print(str + ' ');
 		}
-		
+		System.out.println("\n");
 		/*
 		 * OUTPUT: 
 		 * [Tanisha, Lina] 
 		 * Arthur Tanisha Lina
 		 */
-	}
+		
+		// Arrays.asList() demonstration
+		String arr2[] = {"John", "Marsh", "Becky"};
+		
+		List<String> arrayList2 = Arrays.asList(arr2);
+		
+		System.out.println("Before modification:");
+		System.out.println("ArrayList2: " + arrayList2);
+		System.out.println("\nArray: ");
+		
+		for(String str: arr2) {
+			System.out.print(str+ " ");
+		}
+		System.out.println("\n");
+
+		arr2[0] = "Micah"; // The asList() operation joins the array and the arraylist. 
+						   // A change in the array will also change the arrayList as the ArrayList
+						   // is backed by the array.
+		
+		System.out.println("After modification:");
+		System.out.println("ArrayList2: " + arrayList2);   
+		System.out.println("\nArray: ");
+		
+		for(String str: arr2) {
+			System.out.print(str+ " ");
+		}
+		/*
+		 * Before modification: ArrayList2: [John, Marsh, Becky]
+		 * 
+		 * Array: John Marsh Becky
+		 * 
+		 * After modification: ArrayList2: [Micah, Marsh, Becky]
+		 * 
+		 * Array: Micah Marsh Becky
+		 */	}
 }
